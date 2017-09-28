0b9764e6a5c700b868ad1e425fd3697c131ccdac
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-28 19:26:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,7 +21,6 @@ def create_project_hash
   projects
 end
 
-binding.pry
 # projects: kickstarter.css("li.project.grid_4").first
 # title: project.css("h2.bbcard_name strong a").tex
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
